0e272aee9956e787d8f21aa2826b58b81b114a1b
sanjeetchoudhary2005-glitch <sanjeetchoudhary2005@gmail.com>
2026-07-05 15:26:42 +0530

    merge  remote changes

--- check-next-bin.mjs
+++
@@ -1,9 +0,0 @@
-
-import fs from 'fs';
-import path from 'path';
-const nextBin = path.join('node_modules', 'next', 'dist', 'bin', 'next');
-console.log('Checking next bin path:', nextBin);
-console.log('nextBin exists:', fs.existsSync(nextBin));
-if (fs.existsSync(nextBin)) {
-  console.log('nextBin stats:', fs.statSync(nextBin));
-}
--- list-dir.mjs
+++
@@ -1,4 +0,0 @@
-
-import fs from 'fs';
-console.log('Files in E:\\Snakebet:');
-console.log(fs.readdirSync('E:\\Snakebet', { withFileTypes: true }).map(dirent => `${dirent.isDirectory() ? '[DIR]' : '[FILE]'} ${dirent.name}`));
